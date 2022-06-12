a8d4095d0c4f2f244e1225e6ce7ead4fc8a4dfd0
mswift42 <martin.haesler@gmail.com>
2022-06-12 15:57:04 +0100

    reduce font size.

--- lib/src/pdf/template.dart
+++ lib/src/pdf/template.dart
@@ -6,10 +6,13 @@ import 'package:printing/printing.dart';
 import 'package:qclean/src/rooms/dorm.dart';
 import 'package:qclean/src/rooms/private.dart';
 
-const NONE = 'NONE';
+const none = 'NONE';
 
 Future<Uint8List> generateDocument(
-    List<Dorm> dorms, List<Private> privates) async {
+    List<Dorm> dormcheckouts,
+    List<Private> privatecheckouts,
+    List<Dorm> dormcheckins,
+    List<Private> privatecheckins) async {
   const format = PdfPageFormat.standard;
   final doc = pw.Document(pageMode: PdfPageMode.outlines);
   final font1 = await PdfGoogleFonts.openSansRegular();
@@ -43,55 +46,59 @@ Future<Uint8List> generateDocument(
                   pw.Header(
                       title: 'Good Morning',
                       child: pw.Row(children: [
-                        pw.Text(
+                        _regText(
                           'Good Morning',
                         )
                       ])),
-                  pw.Text('Dorms to strip and make: ${bedList(dorms)} ',
-                      textAlign: pw.TextAlign.left),
-                  pw.Text('Dorms with Check-ins: '),
-                  pw.Text('Privates to strip and make: ${_privates(privates)}'),
-                  pw.Text('Hoover corridors and clean Bathrooms'),
-                  pw.Text('Privates with Check-ins: '),
-                  pw.Bullet(
-                      text:
-                          'CLEAN ALL BATHROOMS 4 bathmats for female showers, 1 for private bathroom'),
-                  pw.Bullet(text: 'CLEAN UPSTAIRS AND DOWNSTAIRS TOILETS'),
-                  pw.Bullet(
-                      text:
-                          'Change hand towels – including private toilet and showers'),
-                  pw.Bullet(text: 'HOOVER ALL CORRIDORS'),
-                  pw.Bullet(text: 'CHECK ALL BINS (CHANGE IF NEEDED)'),
-                  pw.Bullet(text: 'REMOVE ALL RUBBISH FROM DORM ROOMS'),
-                  pw.Bullet(text: 'REFILL ALL TOILET PAPER (IF NECESSARY)'),
-                  pw.Bullet(
-                      text:
-                          'REFILL ALL HAND-SOAP AND SHAMPOO IN PRIVATE BATHROOM'),
-                  pw.Bullet(
-                      text: 'MAKE SURE BEDS WITH ARRIVALS ARE CLEAN AND TIDY'),
-                  pw.Bullet(
-                      text: 'Remove all towels, clothes, etc.  left in rooms.'),
-                  pw.Bullet(
-                      text:
-                          'Check under all beds, and remove any rubbish.  Rooms should be spotless!'),
-                  pw.Bullet(
-                      text: 'Vacuum Reception – PLEASE HOOVER THE TILES, TOO!'),
-                  pw.Bullet(
-                      text:
-                          'Mop Reception, front and back stairs, and area in front of Staff room.  DRY FLOORS!'),
-                  pw.Bullet(text: 'EMPTY THE HAIR TRAPS IN ALL SHOWERS!!'),
-                  pw.Bullet(
-                      text:
-                          'Clean windows around Reception and the common room'),
-                  pw.Bullet(text: 'WIPE ALL DOORHANDLES AND HANDRAILS'),
+                  _regText(
+                      'Dorms to strip and make: ${bedList(dormcheckouts)}'),
+                  _regText('Dorms with Check-ins: ${bedList(dormcheckins)}'),
+                  _regText(
+                      'Privates to strip and make: ${_privates(privatecheckouts)}'),
+                  _regText('Hoover corridors and clean Bathrooms'),
+                  _regText(
+                      'Privates with Check-ins: ${_privates(privatecheckins)}'),
+                  _bulletText(
+                      'CLEAN ALL BATHROOMS 4 bathmats for female showers, 1 for private bathroom'),
+                  _bulletText('CLEAN UPSTAIRS AND DOWNSTAIRS TOILETS'),
+                  _bulletText(
+                      'Change hand towels – including private toilet and showers'),
+                  _bulletText('HOOVER ALL CORRIDORS'),
+                  _bulletText('CHECK ALL BINS (CHANGE IF NEEDED)'),
+                  _bulletText('REMOVE ALL RUBBISH FROM DORM ROOMS'),
+                  _bulletText('REFILL ALL TOILET PAPER (IF NECESSARY)'),
+                  _bulletText(
+                      'REFILL ALL HAND-SOAP AND SHAMPOO IN PRIVATE BATHROOM'),
+                  _bulletText(
+                      'MAKE SURE BEDS WITH ARRIVALS ARE CLEAN AND TIDY'),
+                  _bulletText(
+                      'Remove all towels, clothes, etc.  left in rooms.'),
+                  _bulletText(
+                      'Check under all beds, and remove any rubbish.  Rooms should be spotless!'),
+                  _bulletText(
+                      'Vacuum Reception – PLEASE HOOVER THE TILES, TOO!'),
+                  _bulletText(
+                      'Mop Reception, front and back stairs, and area in front of Staff room.  DRY FLOORS!'),
+                  _bulletText('EMPTY THE HAIR TRAPS IN ALL SHOWERS!!'),
+                  _bulletText(
+                      'Clean windows around Reception and the common room'),
+                  _bulletText('WIPE ALL DOORHANDLES AND HANDRAILS'),
                 ]));
       }));
   return doc.save();
 }
 
+pw.Text _regText(String text) {
+  return pw.Text(text, textScaleFactor: 0.8);
+}
+
+pw.Bullet _bulletText(String text) {
+  return pw.Bullet(text: text, style: const pw.TextStyle(fontSize: 10));
+}
+
 String bedList(List<Dorm> dorms) {
   if (dorms.isEmpty) {
-    return NONE;
+    return none;
   }
   var list = '';
   for (var dorm in dorms) {
@@ -99,19 +106,19 @@ String bedList(List<Dorm> dorms) {
     for (var bed in dorm.beds) {
       list += bed.name;
     }
-    list += ' ';
+    list += ', ';
   }
   return list;
 }
 
 String _privates(List<Private> privates) {
   if (privates.isEmpty) {
-    return NONE;
+    return none;
   }
   var list = '';
   for (var private in privates) {
     list += 'PVT';
-    list += private.number.toString();
+    list += '${private.number.toString()}, ';
   }
   return list;
 }
