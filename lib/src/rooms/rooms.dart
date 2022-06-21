49d2dd7241617b0d1614e282ad0cdd763639b98b
mswift42 <martin.haesler@gmail.com>
2022-06-21 17:55:46 +0100

    add dorms.

---
+++ lib/src/rooms/rooms.dart
@@ -0,0 +1,50 @@
+import 'package:qclean/src/rooms/bed.dart';
+import 'package:qclean/src/rooms/dorm.dart';
+
+Set<Dorm> dorms = {
+  const Dorm(1, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e')]),
+  const Dorm(2, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e')]),
+  const Dorm(3, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e')]),
+  const Dorm(4, [
+    Bed('a'),
+    Bed('b'),
+    Bed('c'),
+    Bed('d'),
+    Bed('e'),
+    Bed('f'),
+    Bed('g'),
+    Bed('h'),
+    Bed('i'),
+    Bed('j')
+  ]),
+  const Dorm(5, [
+    Bed('a'),
+    Bed('b'),
+    Bed('c'),
+    Bed('d'),
+    Bed('e'),
+    Bed('f'),
+    Bed('g'),
+    Bed('h')
+  ]),
+  const Dorm(6, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e'), Bed('f')]),
+  const Dorm(7, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e'), Bed('f')]),
+  const Dorm(8, [Bed('a'), Bed('b'), Bed('c'), Bed('d')]),
+  const Dorm(9, [
+    Bed('a'),
+    Bed('b'),
+    Bed('c'),
+    Bed('d'),
+    Bed('e'),
+    Bed('f'),
+    Bed('g'),
+    Bed('h')
+  ]),
+  const Dorm(10, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e'), Bed('f')]),
+  const Dorm(11, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e'), Bed('f')]),
+  const Dorm(12, [Bed('a'), Bed('b'), Bed('c'), Bed('d')]),
+  const Dorm(13, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e') ]),
+  const Dorm(14, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e')]),
+  const Dorm(15, [Bed('a'), Bed('b'), Bed('c'), Bed('d'), Bed('e')]),
+  const Dorm(16, [Bed('a'), Bed('c')]),
+};
